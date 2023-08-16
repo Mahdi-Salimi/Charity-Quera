c675431a8a78b7763a120b28075bf3151115cbc5
Mahdi-Salimi <mahdisalimi.dev@gmail.com>
2023-08-16 12:01:27 +0330

    Creating custom Admin pannel

--- accounts/models.py
+++ accounts/models.py
@@ -10,14 +10,12 @@ class User(AbstractUser):
     address = models.TextField(blank=True, null=True)
     age = models.PositiveSmallIntegerField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
     first_name = models.CharField(max_length=150, blank=True, null=True)
-    is_superuser = models.BooleanField(default=False)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
     last_login = models.DateTimeField(blank=True, null=True)
     last_name = models.CharField(max_length=150, blank=True, null=True)
-    
+
 
 
 
